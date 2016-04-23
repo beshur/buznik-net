903d81d2f827d2e5f471998d1ad39afa95b3e3ed
Alex Buznik <shu@buznik.net>
2016-04-23 20:55:22 +0300

    Update cron.sh for remote server

--- cron.sh
+++ cron.sh
@@ -1,7 +1,10 @@
 # do some cron:
+cd "$(dirname "$0")"
+command -v php >/dev/null 2>&1 || { echo >&2 "php is undefined. Setting alias."; alias php="/usr/local/bin/php"; }
+
 echo "# Get resume texts:"
 php get_resume_texts.php
-echo "\n"
+echo ""
 echo "# Caching pages:"
 echo "index_rus.html"
 php index_page.php > index_rus.html
