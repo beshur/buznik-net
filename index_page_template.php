dcb0b723d48d85ff0b1367091126bbef187b1484
Alex Buznik <alex.buznik@di.fm>
2019-03-04 21:44:35 +0200

    Update favicon

--- index_page_template.php
+++ index_page_template.php
@@ -9,7 +9,7 @@ if ( !defined('ABSPATH') )
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style type="text/css"><?php include('dist/css/main.css'); ?></style>
-  <link href="favicon.ico?v=1" rel="shortcut icon" />
+  <link href="./favicon.png" rel="shortcut icon" />
 </head>
 
 <body>
