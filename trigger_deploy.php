5077519eea8ffd85cccad5c672e196e8abf2c7f5
Alex Buznik <shu@buznik.net>
2023-11-08 22:17:21 +0200

    fix

--- trigger_deploy.php
+++ trigger_deploy.php
@@ -42,7 +42,9 @@ $tmp_file_name = '_tmp_dist.zip';
 $shell_actions = array(
   'wget ' . DOWNLOAD_URL . ' -O ' . $tmp_file_name,
   'unzip -o ' . $tmp_file_name,
+  'cp -r dist/* ./',
   'rm ' . $tmp_file_name,
+  'sh cron.sh',
   'touch hook_log.txt',
   'sh deploy_postback.sh');
 $output = shell_exec(join($shell_actions, ' && '));
