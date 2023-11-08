cd7e7c3eb0afb50e76ef992d152b2cd7b8fd0fb3
Alex Buznik <shu@buznik.net>
2023-11-08 22:17:21 +0200

    fix

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 command -v php >/dev/null 2>&1 || { echo >&2 "php is undefined. Setting alias."; alias php="/usr/local/bin/php"; }
 
-echo "# Get resume texts:"
+echo "# Get resume texts (1):"
 php get_resume_texts.php
 echo ""
 echo "# Caching pages:"
